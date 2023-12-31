beecd965c002616d61aed3ac07ecfe81c968ff5a
ps018w <premshaw21@gmail.com>
2023-12-31 21:40:44 +0530

    modified

--- accounts/constant.py
+++ accounts/constant.py
@@ -4,3 +4,13 @@ TIMEZONES = [
     ('AEST', 'Australian Eastern Standard Time'),
 ]
 USER_TYPES = [('tutor', 'tutor'), ('student', 'student')]
+
+DAY_OF_WEEK=[
+    ('MON', 'Monday'),
+    ('TUE', 'Tuesday'),
+    ('WED', 'Wednesday'),
+    ('THUR', 'Thursday'),
+    ('FRI', 'Friday'),
+    ('SAT', 'Saturday'),
+    ('SUN', 'Sunday')
+]
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,9 @@
-# from django.db import models
 
-# Create your models here.
-# accounts/models.py
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from .constant import TIMEZONES, USER_TYPES
+from .constant import TIMEZONES, USER_TYPES, DAY_OF_WEEK
+from django.db import models
+from multiselectfield import MultiSelectField
 
 
 class CustomUserManager(BaseUserManager):
@@ -38,4 +37,26 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['full_name']
 
     def __str__(self):
-        return self.email
+        return self.email,
+
+from datetime import datetime
+
+class Calendar(models.Model):
+    user= models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_time = models.TimeField(blank=True)
+    end_time = models.TimeField(blank=True)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    #days = models.Da
+    #slot =
+    days_of_week = MultiSelectField(choices=DAY_OF_WEEK,max_length=7,blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.start_time}", \
+            f"{self.start_time.strftime('%H:%M')} - {self.end_time.strftime('%H:%M')}"
+
+
+
+
