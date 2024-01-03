3cf8a828de7226ac7a9c3f620e55051a4bbb2452
ps018w <premshaw21@gmail.com>
2024-01-03 23:15:19 +0530

    modified

--- accounts/views.py
+++ accounts/views.py
@@ -85,8 +85,20 @@ from dateutil import rrule
 from datetime import datetime
 from rest_framework import viewsets
 from rest_framework.decorators import action
-class CalendarCreateView(viewsets.GenericViewSet):
+class CalendarCreateView(APIView):
     queryset = Calendar.objects.all()
 
-    serializer_class = CalendarSerializer
+    #serializer_class = CalendarSerializer(data=queryset)
 
+    def get(self, request, format=None):
+        user= CustomUser.objects.get(email=request.data['email'])
+        calendar = Calendar.objects.get(user=user)
+        serializer = CalendarSerializer(calendar, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CalendarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
